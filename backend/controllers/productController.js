8c430d2fadfdf225679f4c7add3cdc868d98865d
JayaSurya123456 <js4123587@gmail.com>
2026-01-27 18:14:34 +0530

    'cm'

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 
 const ErrorHandler=require('../utils/errorHandler')
 const Product=require('../models/productModel')
+const catchAsyncError = require('../middlewares/catchAsyncError')
 const APIfeatures= require('../utils/apiFeatures')
 
 //http:localhost:8000/api/v1/products
@@ -75,3 +76,91 @@ exports.deleteProduct=async(req,res,next)=>{
     message:'product deleted'
    })
 }
+
+
+//create review api/v1/review
+
+exports.createReview=catchAsyncError(async(req,res,next)=>{
+    const  { productId, rating, comment } = req.body;
+
+    // ready review object to push 
+    const reviews={
+      user:req.user.id,
+      rating,
+      comment
+    }
+
+   const product=await Product.findById(productId)
+   // check user already reviewed
+   const isReviewed=product.reviews.find((review)=>{
+   return review.user.toString()==req.user.id.toString()
+   })
+
+   if(isReviewed){
+    // update review
+    product.reviews.forEach((review)=>{
+        if(review.user.toString()==req.user.id.toString()){
+             review.comment = comment
+             review.rating = rating
+        }
+    })
+   }
+   else{
+    // add new review
+       product.reviews.push(reviews)
+       product.numOfReviews=product.reviews.length
+   }
+ //find the average of the product reviews
+    product.ratings = product.reviews.reduce((acc, review) => {
+        return review.rating + acc;
+    }, 0) / product.reviews.length;
+    product.ratings = isNaN(product.ratings)?0:product.ratings;
+
+    await product.save({validateBeforeSave: false});
+    res.status(200).json({
+        success: true
+    })
+
+})
+
+//Get reviews query via parameter
+exports.getReviews=catchAsyncError(async (req,res,next)=>{
+const product=await Product.findById(req.query.id)
+  
+  res.status(200).json({
+        success: true,
+        reviews: product.reviews
+    })
+})
+
+
+// delete review
+
+exports.deleteReviews=catchAsyncError(async (req,res,next)=>{
+    const product=await Product.findById(req.query.productId)
+
+    // loop the entire array and store all expect passing id
+    const reviews = product.reviews.filter(review => {
+       return review._id.toString() !== req.query.id.toString()
+    });
+
+    //update length after filter
+    const numOfReviews=reviews.length
+
+    // again finding average for filtered reviews
+    let ratings = reviews.reduce((acc, review) => {
+        return review.rating + acc;
+    }, 0) / reviews.length;
+    ratings = isNaN(ratings)?0:ratings;
+
+    //updating the final ratings noOfReviews and length
+    await Product.findByIdAndUpdate(req.query.productId,{
+        reviews,
+        numOfReviews,
+        ratings
+    })
+
+  res.status(200).json({
+        success: true,
+    })
+})
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,7 +1,10 @@
 
 const express =require('express')
 const { isAuthenticatedUser,authorizeRoles } = require('../middlewares/authenticate')
-const { getProducts, newproduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController')
+const { getProducts, newproduct, getSingleProduct,
+     updateProduct, deleteProduct, createReview, 
+     getReviews,
+     deleteReviews} = require('../controllers/productController')
 const router=express.Router()
 
 router.route('/products').get(isAuthenticatedUser,getProducts)
@@ -12,6 +15,10 @@ router.route('/product/:id')
 
 //admin route
 router.route('admin/product/new').post(isAuthenticatedUser,authorizeRoles('admin'),newproduct)
+
+router.route('/review').post(isAuthenticatedUser,createReview)
+router.route('/getReview').get(isAuthenticatedUser,getReviews)
+router.route('/deleteReview').delete(isAuthenticatedUser,deleteReviews)
 
 
 module.exports=router
